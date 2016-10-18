34254c010e85b73f43e692451f44169757d79ebf
Brady Potaczek <bpotaczek@yahoo.com>
2016-10-17 20:06:24 -0500

    Removed test comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestcomponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
