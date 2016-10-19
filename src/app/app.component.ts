d4cf4efb371e51e244a804e77b75c50c4d62126d
Brady Potaczek <bpotaczek@yahoo.com>
2016-10-19 09:54:57 -0500

    Added comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // what to use to display this component <app-root></app-root>
   selector: 'app-root',
+  // url to template
   templateUrl: './app.component.html',
+  // styles relevent to this component
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,21 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // components, directives and pipes
   declarations: [
     AppComponent
   ],
+  // modules whose public classes you need to reference in templates (e.g. ngIf, ngModel)
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  // components, directives, and pipes used by other modules that will import this module
+  exports: [],
+  // services to register with the app for dependency injection
   providers: [],
+  // component to bootstrap
   bootstrap: [AppComponent]
 })
 export class AppModule { }
